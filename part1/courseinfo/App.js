d228838a222eff3b3621b0d1ef701d81e96a8245
Kiersten Campbell <55903462+colliecoder@users.noreply.github.com>
2023-01-17 13:52:16 -0500

    Added 1.8
    Statistics component

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+
+  let all = props.good + props.neutral + props.bad
+  return (
+    <div>
+      <h1>statistics</h1> 
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {(props.good / all) - (props.bad) / all}</p>
+      <p>positive {(props.good / all) * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let all = good + neutral + bad
 
   return (
     <div>
@@ -14,13 +28,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1> 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good / all) - (bad) / all}</p>
-      <p>positive {(good / all) * 100} %</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     
 
     </div>
